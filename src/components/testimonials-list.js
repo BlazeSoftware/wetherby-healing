799f8bad947367b2fc116aa89bde9b08e891b16b
Greg <gregory.pratt@me.com>
2020-07-16 20:24:42 +0100

    fix: testimonials

--- src/components/testimonials-list.js
+++ src/components/testimonials-list.js
@@ -36,12 +36,11 @@ const TestimonialsList = () => {
   const settings = {
     dots: false,
     fade: true,
-    arrows: false,
     infinite: true,
     speed: 1000,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 20000,
     pauseOnHover: false,
   };
 
